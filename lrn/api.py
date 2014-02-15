11923643b5f1490f6faff26cc7eb94b9470dfe68
Razzi <razzi53@gmail.com>
2014-02-15 14:06:44 -0500

    'set trace'

--- lrn/api.py
+++ lrn/api.py
@@ -2,9 +2,12 @@ import os
 from json import loads
 from subprocess import check_output
 
+from ipdb import set_trace
+
 import requests
 
 def get_repos():
+	set_trace()
 	r = requests.get('https://api.github.com/orgs/lrn-guru/repos')
 	repos = r.json()
 	repo_names = []
