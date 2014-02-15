984ec14f5e7693e19ae3eff642c533bd4acc1468
Alex Brashear <alexjbrashear@gmail.com>
2014-02-14 21:03:57 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='lrn-guru',
+    version='0.0.1',
+    author=['Razzi Abuissa','Alex Brashear'],
+    author_email='alexjbrashear@gmail.com',
+    packages=['towelstuff', 'towelstuff.test'],
+    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.1.1",
+        "caldav == 0.1.4",
+    ],
+)
