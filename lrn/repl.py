febbfe80347b4aca8d9e331f3de2b27acdf14c64
Alex Brashear <alexjbrashear@gmail.com>
2014-02-15 15:23:44 -0500

    repl updated

--- lrn/repl.py
+++ lrn/repl.py
@@ -1,29 +1,15 @@
-from subprocess import check_output
-import termios
-import sys
-import pty
-
-def getpass(prompt="Password: "):
-    fd = sys.stdin.fileno()
-    old = termios.tcgetattr(fd)
-    new = termios.tcgetattr(fd)
-    new[3] = new[3] & ~termios.ECHO          # lflags
-    try:
-        termios.tcsetattr(fd, termios.TCSADRAIN, new)
-        passwd = raw_input(prompt)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old)
-    return passwd
+import os
 
 def main():
 	while True:
 		cmd = raw_input("> ")
-		print(check_output(cmd.split()))
+		if cmd.startswith('vim'):
+			os.system(cmd)
+		else:
+			print('enter vim')
 
 
 if __name__ == '__main__':
-	#passw = getpass("> ")
-	#print(passw)
-	pty.spawn()
+	main()
 	
 
