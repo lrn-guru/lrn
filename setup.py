2042f33a712327e268d7c0aa4ac277a273a24edd
Alex Brashear <alexjbrashear@gmail.com>
2014-02-14 22:35:13 -0500

    finished setting up pip installation

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from distutils.core import setup
 
 setup(
     name='lrn-guru',
-    version='0.0.1',
-    author=['Razzi Abuissa','Alex Brashear'],
+    version='0.0.2',
+    author='Razzi Abuissa + Alex Brashear',
     author_email='alexjbrashear@gmail.com',
     packages=['lrn'],
     scripts=['bin/lrn.py'],
     url='http://pypi.python.org/pypi/TowelStuff/',
     license='LICENSE.txt',
-    description='Useful towel-related stuff.',
+    description='The command line interface for the lrn tutorial platform',
     long_description=open('README.md').read(),
-    install_requires=[
-        "Django>=1.1.1",
-        "caldav==0.1.4",
-    ],
+	install_requires=[],
 )
+
