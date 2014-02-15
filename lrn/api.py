060f142cc412ce010074e875b36a75d489c420d7
Razzi <razzi53@gmail.com>
2014-02-15 12:20:44 -0500

    'get_current_repo'

--- lrn/api.py
+++ lrn/api.py
@@ -1,5 +1,18 @@
 import requests
-import json
+import os
+
+from pygit2 import Repository
+
+def get_current_repo():
+	if os.path.exists('.git'):
+		return Repository('.')
+	elif os.path.exists('../.git'):
+		return Repository('..')
+	elif os.path.exists('../../.git'):
+		return Repository('../..')
+	else:
+		print('No git repository found.')
+		exit(1)
 
 def get_repos():
 	r = requests.get('https://api.github.com/orgs/lrn-guru/repos')
@@ -14,4 +27,3 @@ def get_repos():
 def get_config(name):
 
 	r = requests.get('https://api.github.com/orgs/lrn-guru/repos')
-	
