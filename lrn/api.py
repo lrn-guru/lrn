370c3fbeaf6321e98e66ea3ae1c502693ca99138
Razzi <razzi53@gmail.com>
2014-02-15 19:09:32 -0500

    'switch to spaces'

--- lrn/api.py
+++ lrn/api.py
@@ -7,74 +7,76 @@ from subprocess import check_output
 import requests
 
 def get_repos():
-	# set_trace()
-	r = requests.get('https://api.github.com/orgs/lrn-guru/repos')
-	repos = r.json()
-	# repos = loads(open('~/repos.json', 'r').read())
-	repo_names = []
-	for repo in repos:
-		name = repo['full_name'].split('/')
-		project_name = name[1]
-		if (project_name == 'lrn' or project_name == 'lrn-guru.github.io'):
-			continue
-		github_name = project_name[6:]
-		js = get_config(github_name)
-		description = js['short_description']
-		difficulty = js['difficulty']
-		repo_names.append((github_name, description, difficulty))
+    # set_trace()
+    r = requests.get('https://api.github.com/orgs/lrn-guru/repos')
+    repos = r.json()
+    # repos = loads(open('~/repos.json', 'r').read())
+    repo_names = []
+    for repo in repos:
+        name = repo['full_name'].split('/')
+        project_name = name[1]
+        if (project_name == 'lrn' or project_name == 'lrn-guru.github.io'):
+            continue
+        github_name = project_name[6:]
+        js = get_config(github_name)
+        description = js['short_description']
+        difficulty = js['difficulty']
+        repo_names.append((github_name, description, difficulty))
 
-	return repo_names
+    return repo_names
 
 
 def get_config(name):
-	url = 'https://raw.github.com/lrn-guru/learn-{}/master/.config.json'.format(name)
-	r = requests.get(url)
-	return r.json()
+    url = 'https://raw.github.com/lrn-guru/learn-{}/master/.config.json'.format(name)
+    r = requests.get(url)
+    return r.json()
 
 
 def get_local_config():
-	if os.path.exists('.config.json'):
-		path = ''
-	elif os.path.exists('../.config.json'):
-		path = '../'
-	else:
-		path = '../../'
-	with open('{}.config.json'.format(path), 'r') as js:
-		return loads(js.read())
+    if os.path.exists('.config.json'):
+        path = ''
+    elif os.path.exists('../.config.json'):
+        path = '../'
+    else:
+        path = '../../'
+    with open('{}.config.json'.format(path), 'r') as js:
+        return loads(js.read())
 
 
 def get_task():
-	branch = get_branch()
-	config = get_local_config()
-	for lesson in config['lessons']:
-		if lesson['branch'] == branch:
-			break # at the right lesson
+    branch = get_branch()
+    config = get_local_config()
+    for lesson in config['lessons']:
+        if lesson['branch'] == branch:
+            break # at the right lesson
 
-	lrn_task = get_lrn_task()
+    lrn_task = get_lrn_task()
 
-	return lesson['tasks'][lrn_task]
+    return lesson['tasks'][lrn_task]
 
 def get_branch():
-	status = check_output('git branch'.split())
-	return status[2:status.find('\n')]
+    status = check_output('git branch'.split())
+    return status[2:status.find('\n')]
 
 def get_starting_branch():
-	config = get_local_config()
-	return config['lessons'][0]['branch']
+    config = get_local_config()
+    return config['lessons'][0]['branch']
 
 def find_lrn_txt():
-	this_dir = '.lrn_task.txt'
-	for i in range(4):
-		path = '..' * i + this_dir
-		if os.path.exists(path):
-			return path
+    this_dir = '.lrn_task.txt'
+    for i in range(4):
+        path = '..' * i + this_dir
+        if os.path.exists(path):
+            __import__('ipdb').set_trace()
+            return path
+    # If it was not found,
 
 def set_lrn_task(number):
-	lrn_json = find_lrn_txt()
-	with open(lrn_json, 'w') as f:
-		f.write(number)
+    lrn_json = find_lrn_txt()
+    with open(lrn_json, 'w') as f:
+        f.write(number)
 
 def get_lrn_task():
-	lrn_json = find_lrn_txt()
-	with open(lrn_json, 'r') as f:
-		return int(f.read())
+    lrn_json = find_lrn_txt()
+    with open(lrn_json, 'r') as f:
+        return int(f.read())
--- lrn/lrn.py
+++ lrn/lrn.py
@@ -12,12 +12,12 @@ from subprocess import call
 import api
 
 def l(s, color=None, newline=True):
-	''' Print a string with a color, no endline.'''
-	opts = {'end': '\n' if newline else ''}
-	if color:
-		cprint(s, color, **opts)
-	else:
-		print(s, **opts)
+    ''' Print a string with a color, no endline.'''
+    opts = {'end': '\n' if newline else ''}
+    if color:
+        cprint(s, color, **opts)
+    else:
+        print(s, **opts)
 
 
 description = 'lrn -- the interactive tutorial platform'
@@ -32,66 +32,66 @@ projects = api.get_repos()
 
 
 def list_projects():
-	l('id'.ljust(9), 'white', False)
-	l('description'.ljust(58), 'white', False)
-	l('difficulty', 'white')
-	l('-' * 73)
-	for proj in projects:
-		l(proj[0].ljust(9), 'yellow', False)
-		l(proj[1].ljust(60), 'blue', False)
-		l('*' * proj[2], 'yellow')
+    l('id'.ljust(9), 'white', False)
+    l('description'.ljust(58), 'white', False)
+    l('difficulty', 'white')
+    l('-' * 73)
+    for proj in projects:
+        l(proj[0].ljust(9), 'yellow', False)
+        l(proj[1].ljust(60), 'blue', False)
+        l('*' * proj[2], 'yellow')
 
 def introduce():
-	""" Welcomes a new user to the {name} tutorial."""
-	config = api.get_local_config()
-	l(config['introduction'], 'green')
+    """ Welcomes a new user to the {name} tutorial."""
+    config = api.get_local_config()
+    l(config['introduction'], 'green')
 
 
 def task(number=None):
-	""" Informs the user of their current task."""
-	# If the task number is not none, ask the api for that
-	task = api.get_task(number)
-	l(task['instruction'], 'cyan')
+    """ Informs the user of their current task."""
+    # If the task number is not none, ask the api for that
+    task = api.get_task(number)
+    l(task['instruction'], 'cyan')
 
 
 
 def start(name):
-	url = 'https://github.com/lrn-guru/learn-{}.git'.format(name)
-	command = 'git clone {}'.format(url)
-	call(command.split())
-	folder = 'learn-{}'.format(name)
-	os.system('cd {}'.format(folder))
-	os.chdir(folder)
-	api.set_lrn_task(0)
+    url = 'https://github.com/lrn-guru/learn-{}.git'.format(name)
+    command = 'git clone {}'.format(url)
+    call(command.split())
+    folder = 'learn-{}'.format(name)
+    os.system('cd {}'.format(folder))
+    os.chdir(folder)
+    api.set_lrn_task(0)
 
-	branch = api.get_starting_branch()
-	command = 'git checkout {}'.format(branch)
-	os.system(command)
-	introduce()
-	task()
+    branch = api.get_starting_branch()
+    command = 'git checkout {}'.format(branch)
+    os.system(command)
+    introduce()
+    task()
 
 
 def run_tests():
-	# get the current branch
-	task = api.get_task()
-	test = task['test']
+    # get the current branch
+    task = api.get_task()
+    test = task['test']
 
-	print(test)
-	# test_file =
+    print(test)
+    # test_file =
 
 def hint():
-	task_json = api.get_task()
-	print(task_json['hint'])
+    task_json = api.get_task()
+    print(task_json['hint'])
 
 
 if args.command == 'list':
-	list_projects()
+    list_projects()
 elif args.command == 'start':
-	if len(sys.argv) == 3:
-		start(sys.argv[-1])
-	else:
-		l('Error: Specify a tutorial to start.', 'red')
+    if len(sys.argv) == 3:
+        start(sys.argv[-1])
+    else:
+        l('Error: Specify a tutorial to start.', 'red')
 elif args.command == 'hint':
-	hint()
+    hint()
 else:
-	print('!')
+    print('!')
