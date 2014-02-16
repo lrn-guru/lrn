badf0f7b37afbdc3d238e227a8bdfa61eaf86208
Razzi <razzi53@gmail.com>
2014-02-16 04:44:11 -0500

    'add init to modules'

---
+++ lrn/api/__init__.py
@@ -0,0 +1,12 @@
+from .api import (
+    l,
+    get_repos,
+    get_local_config,
+    get_config,
+    get_tests_dir,
+    get_task,
+    get_branch,
+    get_starting_branch,
+    find_lrn_txt,
+    set_lrn_task
+)
