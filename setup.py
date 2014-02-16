0d30265055328dc258349ee2cb511a47b5a4de36
Alex Brashear <alexjbrashear@gmail.com>
2014-02-16 04:35:24 -0500

    attempting to fix bugs

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='lrn',
-    version='0.0.8',
+    version='0.1.0',
     author='Razzi Abuissa + Alex Brashear + Peter Yin',
     author_email='py.peteryin@gmail.com',
     url='https://github.com/lrn-guru',
-    packages=['lrn'],
+    packages=['lrn', 'lrn.api', 'lrn.repl'],
     scripts=['lrn/lrn'],
 )
 
